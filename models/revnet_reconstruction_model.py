d397626c15af1989439d0d20c8df13b32477140a
Yuval Uner <yuvaluner@gmail.com>
2024-05-23 17:32:59 +0300

    changed default lambda values for revnet_reconstruction_model.py

--- models/revnet_reconstruction_model.py
+++ models/revnet_reconstruction_model.py
@@ -37,8 +37,8 @@ class RevnetReconstructionModel(BaseModel):
         """
         parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
         if is_train:
-            parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
-            parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
+            parser.add_argument('--lambda_A', type=float, default=1.0, help='weight for cycle loss (A -> B -> A)')
+            parser.add_argument('--lambda_B', type=float, default=1.0, help='weight for cycle loss (B -> A -> B)')
         return parser
 
     def __init__(self, opt):
